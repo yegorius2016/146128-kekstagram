d4ff93c2bf0945e595f90c8f4a94c0c6d2b1b419
yegorius2016 <egorius2002@yahoo.com>
2016-05-26 22:23:03 +0800

    Update check.js

--- build/js/check.js
+++ build/js/check.js
@@ -1,20 +1,31 @@
-var a;
-var b;
 function getMessage(a:*, b:*=):string {
-  if (typeof(a) === 'boolean') {
-    if (a = true) {
-    alert('Переданное GIF-изображение анимировано и содержит' + ' ' + b + 'кадров');
+  if (typeof a === 'boolean') {
+    if (a === true) {
+    return 'Переданное GIF-изображение анимировано и содержит' + ' ' + b + 'кадров';
     }
     else {
-    alert('Переданное GIF-изображение не анимировано');
+    return 'Переданное GIF-изображение не анимировано';
     }
-  if (typeof(a) === 'number') {
-    alert ('Переданное SVG-изображение содержит' + a + 'объектов и' + (b * 4) + ' ' + 'атрибутов');
   }
-  if ((a = [sum]) && (a instanceof Array) {
-    alert ()
+  if (typeof a === 'number') {
+    return 'Переданное SVG-изображение содержит' + a + 'объектов и' + (b * 4) + ' ' + 'атрибутов';
   }
-  
-  
+  if (Array.isArray(a)) {
+    for var i = 0; i < a.length; i++ {
+      var array = [];
+      var sum = 0;
+      sum += array[i];
+    }
+    return 'Количество красных точек во всех строчках изображения: ' + sum;
+  }
+  if ((Array.isArray(a) && Array.isArray(b)) {
+    for var i = 0; i < a.length; i++ {
+      var array = [];
+      var square = 0;
+      square = (a[i] * b[i]);
+    }
+  return 'Общая площадь артефактов сжатия: ' + square + 'пикселей';
+  }
 } 
+
 
