3006d9a7acc77ec727651d65827fdf8c87cbcbd5
Yegorius <egorius2002@yahoo.com>
2016-05-27 23:36:41 +0800

    Update check.js

--- build/js/check.js
+++ build/js/check.js
@@ -1,21 +1,21 @@
 var getMessage = function(a, b) {
   if (typeof a === 'boolean') {
-    if (a == true) {
-    return('Переданное GIF-изображение анимировано и содержит' + ' ' + b + ' ' + 'кадров');
+    if (a) {
+      return('Переданное GIF-изображение анимировано и содержит ' + b + ' кадров');
     }
-    else if (a == false) {
-    return('Переданное GIF-изображение не анимировано');
+    else {
+      return('Переданное GIF-изображение не анимировано');
     }
   }
   if (typeof a === 'number') {
-    return('Переданное SVG-изображение содержит' + ' ' + a + ' ' + 'объектов и' + ' ' + (b * 4) + ' ' + 'аттрибутов');
+    return('Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' аттрибутов');
   }
   if (Array.isArray(a) && (!Array.isArray(b))) {
     var sum = 0;
     for (var i = 0; i < a.length; i++) {
       sum += a[i];
     }
-    return('Количество красных точек во всех строчках изображения:' + ' ' + sum);
+    return('Количество красных точек во всех строчках изображения: ' + sum);
   }
   if (Array.isArray(a) && Array.isArray(b)) {
     var square = 0;
