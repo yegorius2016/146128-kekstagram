4547de9f578f83d3d965394f446ffaabc66e62ae
Yegorius <egorius2002@yahoo.com>
2016-05-27 17:47:15 +0800

    Update check.js

--- build/js/check.js
+++ build/js/check.js
@@ -12,14 +12,14 @@ var getMessage = function(a, b) {
   }
   if (Array.isArray(a)) {
     var sum = 0;
-    for(var i = 0; i < a.length; i++) {
+    for (var i = 0; i < a.length; i++) {
       sum += a[i];
     }
     return('Количество красных точек во всех строчках изображения:' + ' ' + sum);
   }
   if (Array.isArray(a) && Array.isArray(b)) {
     var square = 0;
-    for(var i = 0; i < a.length; i++) {
+    for (var i = 0; i < a.length && i < b.length; i++) {
       square += a[i] * b[i];
     }
     return('Общая площадь артефактов сжатия:' + ' ' + square + ' ' + 'пикселей');
