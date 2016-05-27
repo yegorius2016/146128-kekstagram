7d9b811c8ee34edd39d43d6aa97495865fb296df
Yegorius <egorius2002@yahoo.com>
2016-05-27 19:09:09 +0800

    Update check.js

--- build/js/check.js
+++ build/js/check.js
@@ -11,21 +11,18 @@ var getMessage = function(a, b) {
     return('Переданное SVG-изображение содержит' + ' ' + a + ' ' + 'объектов и' + ' ' + (b * 4) + ' ' + 'аттрибутов');
   }
   if (Array.isArray(a)) {
+    var sum = 0;
     for (var i = 0; i < a.length; i++) {
-      var sum = 0;
-      sum = sum + a[i];
       sum += a[i];
-      return('Количество красных точек во всех строчках изображения:' + ' ' + sum);
     }
+    return('Количество красных точек во всех строчках изображения:' + ' ' + sum);
   }
   if (Array.isArray(a) && Array.isArray(b)) {
+    var square = 0;
     for(var i = 0; i < a.length; i++) {
-      var arr = [];
-      arr.push(a[i] * b[i]);
-      var square = 0;
-      square = square + arr[i];
-      return('Общая площадь артефактов сжатия:' + ' ' + square + ' ' + 'пикселей');
+      square += (a[i] * b[i]);
     }
+    return('Общая площадь артефактов сжатия:' + ' ' + square + ' ' + 'пикселей');
   }
 }
 
