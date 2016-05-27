b4568127ac3af8ec0f9c2d44dff3896aa134c454
Yegorius <egorius2002@yahoo.com>
2016-05-27 09:15:06 +0800

    Update check.js

--- build/js/check.js
+++ build/js/check.js
@@ -1,6 +1,5 @@
-function getMessage() {
-  var a = 0;
-  var b = 0;
+function getMessage(a, b) {
+  return (a:*, b:*=):string;
   if (typeof a === 'boolean') {
     if (a == true) {
     return('Переданное GIF-изображение анимировано и содержит' + ' ' + b + ' ' + 'кадров');
@@ -27,6 +26,6 @@ function getMessage() {
     return('Общая площадь артефактов сжатия:' + ' ' + square + ' ' + 'пикселей');
   }
 } 
-getMessage(a:*, b:*=):string;
+
 
 
