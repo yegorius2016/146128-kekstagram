2fff5f421c1ad02018a566368d05cf965b7e0dbe
Yegorius <egorius2002@yahoo.com>
2016-05-27 09:44:44 +0800

    Update check.js

--- build/js/check.js
+++ build/js/check.js
@@ -1,30 +1,29 @@
 function getMessage((a:*, b:*=):string) {
-  
-}
-if (typeof a === 'boolean') {
-  if (a == true) {
-  return('Переданное GIF-изображение анимировано и содержит' + ' ' + b + ' ' + 'кадров');
+  if (typeof a === 'boolean') {
+    if (a == true) {
+    return('Переданное GIF-изображение анимировано и содержит' + ' ' + b + ' ' + 'кадров');
+    }
+    else if (a == false) {
+    return('Переданное GIF-изображение не анимировано');
+    }
   }
-  else if (a == false) {
-  return('Переданное GIF-изображение не анимировано');
+  if (typeof a === 'number') {
+    return('Переданное SVG-изображение содержит' + ' ' + a + ' ' + 'объектов и' + ' ' + (b * 4) + ' ' + 'атрибутов');
   }
-}
-if (typeof a === 'number') {
-  return('Переданное SVG-изображение содержит' + ' ' + a + ' ' + 'объектов и' + ' ' + (b * 4) + ' ' + 'атрибутов');
-}
-if (Array.isArray(a)) {
-  var sum = 0;
-  for var i = 0; i < a.length; i++ {
-    sum += array[i];
+  if (Array.isArray(a)) {
+    var sum = 0;
+    for var i = 0; i < a.length; i++ {
+      sum += array[i];
+    }
+    return('Количество красных точек во всех строчках изображения:' + ' ' + sum);
   }
-  return('Количество красных точек во всех строчках изображения:' + ' ' + sum);
-}
-if ((Array.isArray(a) && Array.isArray(b)) {
-  var square = 0;
-  for var i = 0; i < a.length; i++ {
-    square += a[i] * b[i];
+  if ((Array.isArray(a) && Array.isArray(b)) {
+    var square = 0;
+    for var i = 0; i < a.length; i++ {
+      square += a[i] * b[i];
+    }
+    return('Общая площадь артефактов сжатия:' + ' ' + square + ' ' + 'пикселей');
   }
-  return('Общая площадь артефактов сжатия:' + ' ' + square + ' ' + 'пикселей');
 }
  
 
