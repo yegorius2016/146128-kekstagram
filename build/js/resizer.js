01930f7e14bde1c7fa427cbba848acc24c90aba0
Yegorius <egorius2002@yahoo.com>
2016-05-29 23:34:11 +0800

    Update resizer.js

--- build/js/resizer.js
+++ build/js/resizer.js
@@ -81,8 +81,7 @@
      */
     redraw: function() {
       // Очистка изображения.
-      this._ctx.fillRect(0, 0, this._container.width, this._container.height);
-      this._ctx.globalAlpha = 0.8;
+      this._ctx.clearRect(0, 0, this._container.width, this._container.height);
       
       // Параметры линии.
       // NB! Такие параметры сохраняются на время всего процесса отрисовки
